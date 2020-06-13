59650329fef7e374054d19ddd073dd6ad0c93de3
Satyam Seth <satyam1998.1998@gmail.com>
2020-06-12 17:17:03 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+A simple todo django app 
+
+How to Run- 
+1.Open cmd or terminal 
+2.Go to file location were manage.py is located 
+3.Run the command "python manage.py runserver" 
+4.Open your browser and open url "http://127.0.0.1:8000/"
+
+Django Admin Login dtails-
+superuser- root
+password- root
---
+++ todoapp/main/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.utils import timezone
+from django.http import HttpResponseRedirect
+from .models import Todo
+
+# Create your views here.
+
+def home(request):
+    todo_items=Todo.objects.all().order_by('added_date')
+    return render(request,'main/index.html',{'todo_items':todo_items})
+
+def add_todo(request):
+    current_date=timezone.now()
+    content=request.POST['content']
+    print(content)
+    created_obj=Todo.objects.create(added_date=current_date,text=content)
+    length_of_todos=Todo.objects.all().count()
+    return HttpResponseRedirect('/')
+
+def delete_todo(request,todo_id):
+    Todo.objects.get(id=todo_id).delete()
+    return HttpResponseRedirect('/')
